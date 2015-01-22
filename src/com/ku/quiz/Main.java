a61a9fa7a404ab71d0cfa02c4efda4f14cb3c15c
new08242 <new_kswiss@hotmail.com>
2015-01-22 12:56:49 +0800

    Update Main.java
    Add Nathas name.

--- src/com/ku/quiz/Main.java
+++ src/com/ku/quiz/Main.java
@@ -12,6 +12,7 @@ public class Main {
 		// add team members here.
 		team.addMember("5610545757", "Manatsawin Hanmongkolchai");
 		team.addMember("5610545676", "Nut Kaewnak");
+		team.addMember("5610546753", "Nathas Yingsukamol");
 		
 		team.displayMembers();
 	}
