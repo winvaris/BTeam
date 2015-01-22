0e3db0343e7cc03f003e7b9fc23f6ec55f0e7da5
mapfap <mapfaps@gmail.com>
2015-01-22 09:28:16 +0700

    initial commit

---
+++ src/com/ku/quiz/Main.java
@@ -0,0 +1,17 @@
+package com.ku.quiz;
+
+public class Main {
+	
+	/**
+	 * Add team members and display it.
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		Team team = Team.getInstance();
+		
+		// add team members here.
+		
+		team.displayMember();
+	}
+	
+}
---
+++ src/com/ku/quiz/Team.java
@@ -0,0 +1,40 @@
+package com.ku.quiz;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Team {
+	
+	private Map<String, String> members;
+	private static Team instance;
+	
+	private Team() {
+		members = new HashMap<String, String>();
+	}
+	
+	public static Team getInstance() {
+		if (instance == null) {
+			instance = new Team();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Add a new member to team.
+	 * @param id id of a new member.
+	 * @param name name of a new member.
+	 */
+	public void addMember(String id, String name) {
+		members.put(id, name);
+	}
+	
+	/**
+	 * Display all team members on screen.
+	 */
+	public void displayMember() {
+		System.out.println("id          name");
+		for (String id: members.keySet()) {
+			System.out.println(id + "  " + members.get(id));
+		}
+	}
+}
